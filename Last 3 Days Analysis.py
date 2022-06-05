788cdc0f4422b5ecc95a1678b10031979b31dd74
MaximillianTam <55353999+MaximillianTam@users.noreply.github.com>
2022-06-05 10:16:46 -0400

    Add files via upload

--- Last 3 Days Analysis.py
+++ Last 3 Days Analysis.py
@@ -1,9 +1,13 @@
 import openpyxl 
 import pandas as pd
+import random
 
-# df = pd.read_excel('C:/Users/mysim/OneDrive/Desktop/My Simple Essential - The Anti Case-Ad Report(2022-05-03 to 2022-05-06).xlsx')
+#Keep this file 
+# df = pd.read_excel('C:/Users/mysim/OneDrive/Desktop/Samsung 2022 Jan Ads.xlsx')
 
-df = pd.read_excel('C:/Users/mysim/OneDrive/Desktop/My Simple Essential - The Anti Case-Ad Report(2022-05-11 to 2022-05-14) (1).xlsx')
+# df = pd.read_excel('C:/Users/mysim/OneDrive/Desktop/My Simple Essential - The Anti Case-Ad Report(2022-05-27 to 2022-05-30).xlsx')
+
+df = pd.read_excel('C:/Users/mysim/OneDrive/Desktop/My Simple Essential - The Anti Case-Ad Report(2022-05-31 to 2022-06-02) (1).xlsx')
 
 df[['Ad Name', 'B']] = df['Ad Name'].str.split('-', 1, expand=True)
 
@@ -23,11 +27,10 @@ output = pd.pivot_table(df,
 output['CPA'] = round(output['Cost']/output['Total Complete Payment'],2)
 cpa_Table_ads = output[(output['Cost'] != 0)]
 cpa_Table_ads.drop(cpa_Table_ads.tail(1).index,inplace=True)
-
 cpa_Table_ads['CPA'].sort_values(ascending=True)
-
-print(cpa_Table_ads) 
-
+total = cpa_Table_ads['Cost'].sum()
+cpa_Table_ads['Cost % to TTL']  = round((cpa_Table_ads['Cost'])/(total) * 100)
+print(cpa_Table_ads[['Cost','Cost % to TTL','CPA','Total Complete Payment']])
 
 #Pivot Table for Ad Groups
 output2 = pd.pivot_table(df,
@@ -42,26 +45,24 @@ output2 = pd.pivot_table(df,
 output2['CPA'] = round(output2['Cost']/output2['Total Complete Payment'],2)
 cpa_Table_adgroups = output2[(output2['Cost'] != 0)]
 cpa_Table_adgroups.drop(cpa_Table_adgroups.tail(1).index,inplace=True)
+cpa_Table_adgroups = cpa_Table_adgroups.reset_index(level = 0)
+cpa_Table_adgroups = cpa_Table_adgroups.reset_index(level = 0)
 print(cpa_Table_adgroups)
 
 output3 = pd.pivot_table(df,
     index = ['Ad Group Name','Ad Name'],
-    # columns = 'CATEGORY',
     values = ['Cost','Total Complete Payment'],
     aggfunc = 'sum',
     fill_value=0
     )
 output3['CPA'] = round(output3['Cost']/output3['Total Complete Payment'],2)
-# print(output3.columns)
-# output3 = output3.reset_index(level=0)
-# print(output3.columns)
+
 
 ad_adgroupcombo = output3[(output3['Cost'] != 0) ]
-# output3[output3['CPA'].sort_values(ascending=True)]
+# ad_adgroupcombo = ad_adgroupcombo.reset_index(level = 0)
 
-# print(ad_adgroupcombo)
-ad_adgroupcombo.to_excel('C:/Users/mysim/OneDrive - LVMH Fashion Group/Reporting/MISC/Ad Performance 05.10.xlsx', index = False, header=True)
+# cpa_Table_ads.to_excel('C:/Users/mysim/OneDrive/Desktop/Manual Bid Ad Performance.xlsx', index = False, header=True)
+# ad_adgroupcombo.to_excel('C:/Users/mysim/OneDrive/Desktop/Manual Bid Ad Performance.xlsx', index = False, header=True)
 
-# ad_adgroupcombo = ad_adgroupcombo.reset_index(level=0)
 
 print(ad_adgroupcombo)
